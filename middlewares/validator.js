121050308afbe55091fb4bc91b7e826004f9c044
Vova Gridnev <gridnevmami@gmail.com>
2021-11-24 20:53:29 +0300

    bugfix: validator user schema

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -35,7 +35,7 @@ const validationSignup = celebrate({
 const updateUser = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
   }),
 });
 

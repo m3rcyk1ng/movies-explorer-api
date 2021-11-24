ac8e12779aa497dc3b83d42ff5ecc0aca60246b3
Vova Gridnev <gridnevmami@gmail.com>
2021-11-24 19:07:36 +0300

    refactor: refactor app.js on new properties

--- app.js
+++ app.js
@@ -1,118 +1,32 @@
-require('dotenv').config();
-
-const { PORT = 3001 } = process.env;
+const { DATA_BASE, PORT } = require('./config');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const mongoose = require('mongoose');
-const {
-  celebrate,
-  Joi,
-  errors,
-} = require('celebrate');
-const routerUser = require('./routes/users');
-const routerCard = require('./routes/movies');
-const { login, postUser } = require('./contollers/users');
-const auth = require('./middlewares/auth');
-
-const {
-  requestLogger,
-  errorLogger,
-} = require('./middlewares/logger');
-
+const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { options } = require('./utils/constants');
 const app = express();
+const router = require('./routes/index');
 const NotFoundError = require('./errors/not-found-error');
-const { isUrl } = require('./isurl/isurl');
-
+const errorsModule = require('./middlewares/errorsModule');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect(DATA_BASE, {
   useNewUrlParser: true,
 });
 
-const options = {
-  origin: [
-    'http://localhost:3000',
-    'https://debugger.nomoredomains.rocks',
-    'http://debugger.nomoredomains.rocks',
-    'https://m3rcyking.github.io',
-  ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-  credentials: true,
-};
-
 app.use('*', cors(options));
-
 app.use(cookieParser());
-
 app.use(requestLogger);
-
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
-app.post('/signin', celebrate({
-  body: Joi.object()
-    .keys({
-      email: Joi.string()
-        .required()
-        .email(),
-      password: Joi.string()
-        .required()
-        .min(8),
-    }),
-}), login);
-
-app.post('/signup', celebrate({
-  body: Joi.object()
-    .keys({
-      email: Joi.string()
-        .required()
-        .email(),
-      password: Joi.string()
-        .required()
-        .min(8),
-      name: Joi.string()
-        .min(2)
-        .max(30),
-      about: Joi.string()
-        .min(2)
-        .max(30),
-      avatar: Joi.string()
-        .custom(isUrl),
-    }),
-}), postUser);
-
-app.use(auth);
-app.use(routerUser);
-app.use(routerCard);
-
+app.use(router);
 app.use((req, res, next) => {
   next(new NotFoundError('Ресурс не найден'));
 });
-
 app.use(errorLogger);
-
 app.use(errors());
-
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
-  next();
-});
+app.use(errorsModule);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);

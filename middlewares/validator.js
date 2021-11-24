92bc11a8a931180b8d331cf2a1a92087c68d01e0
Vova Gridnev <gridnevmami@gmail.com>
2021-11-24 18:55:24 +0300

    feature: add validator.js

---
+++ middlewares/validator.js
@@ -0,0 +1,70 @@
+const {
+  celebrate,
+  Joi,
+} = require('celebrate');
+const { isUrl } = require('../utils/isurl');
+
+const validationLogin = celebrate({
+  body: Joi.object()
+    .keys({
+      email: Joi.string()
+        .required()
+        .email(),
+      password: Joi.string()
+        .required()
+        .min(8),
+    }),
+});
+
+const validationSignup = celebrate({
+  body: Joi.object()
+    .keys({
+      email: Joi.string()
+        .required()
+        .email(),
+      password: Joi.string()
+        .required()
+        .min(8),
+      name: Joi.string()
+        .required()
+        .min(2)
+        .max(30),
+    }),
+});
+
+const updateUser = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  })
+});
+
+const validationPostMovie = celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().custom(isUrl),
+    trailer: Joi.string().required().custom(isUrl),
+    thumbnail: Joi.string().required().custom(isUrl),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+});
+
+const validationDeleteMovie = celebrate({
+  params: Joi.object().keys({
+    MovieId: Joi.string().required().hex().length(24),
+  }),
+});
+
+module.exports = {
+  validationLogin,
+  validationSignup,
+  updateUser,
+  validationPostMovie,
+  validationDeleteMovie
+};
